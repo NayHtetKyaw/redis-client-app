8dcadc22a1a2e70a6df7a6b807f4b34eec7fe258
NayHtetKyaw <nayhtetkyaw.dev@gmail.com>
2024-01-07 20:16:29 +0700

    >

--- src/pages/api/postform.ts
+++ src/pages/api/postform.ts
@@ -1,7 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { createClient } from "redis";
-import { promisify } from "util";
-
 
 const client = createClient({
   url: "redis://localhost:6379",
@@ -12,10 +10,6 @@ client.on("error", (err) => console.log("Redis Client Error", err));
 client.on("ready", () => {
   console.log("Redis client connected");
 });
-
-// const RedisSetAsync = promisify(client.set).bind(client);
-// const RedisGetAsync = promisify(client.get).bind(client);
-
 
 type ResponseData = {
   message: string;
@@ -31,7 +25,6 @@ export default async function handler(
     const { inputKey, inputValue } = req.body;
 
     await client.set(inputKey, JSON.stringify(inputValue));
-    // console.log("Redis get:", await RedisGetAsync(inputKey));
     res.status(200).json({ message: "It was a success!" });
   
   } catch (error) {
